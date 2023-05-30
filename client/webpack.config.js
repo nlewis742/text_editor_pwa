4a9c242975e18a501fe66b1b12fba6d87b8caf1f
Natasha <nlackas12@gmail.com>
2023-05-29 21:32:08 -0500

    npm i

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -62,7 +62,7 @@ module.exports = () => {
               loader: 'babel-loader',
               options: {
                 presets: ['@babel/preset-env'],
-                plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-transform-runtime']
+                plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread']
               }
           }
         }
